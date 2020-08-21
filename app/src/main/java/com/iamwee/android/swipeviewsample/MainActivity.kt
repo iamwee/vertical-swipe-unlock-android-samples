d5e48e9f782f10638caed30883af88d514e444ee
iamwee <wathin.v@gmail.com>
2020-08-21 12:00:38 +0700

    Integrate with UI

--- app/src/main/java/com/iamwee/android/swipeviewsample/MainActivity.kt
+++ app/src/main/java/com/iamwee/android/swipeviewsample/MainActivity.kt
@@ -2,10 +2,27 @@ package com.iamwee.android.swipeviewsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        unLockerSwipeVerticalView.setOnProgressChangeListener {
+            textView.alpha = (it.toFloat() / 100)
+        }
+
+        unLockerSwipeVerticalView.setOnSwipeActionListener(object : UnLockerSwipeVerticalView.OnSwipeActionListener {
+            override fun onSwipeSucceeded() {
+                Toast.makeText(this@MainActivity, "Swipe succeeded", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onSwipeFailed() {
+                Toast.makeText(this@MainActivity, "Swipe failed", Toast.LENGTH_SHORT).show()
+            }
+
+        })
     }
 }
